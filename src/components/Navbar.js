c6579895a825b2d2c70e057b88a1c10bf7efb582
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-02-09 20:04:27 +0100

    upd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Navbar(props) {
 	return (
 		<header>
 			<nav className="mainNav">
 				<h1>Where in the world?</h1>
 				<label htmlFor="themeSwitcher">
+					<span>
+						<FontAwesomeIcon icon="fa-light fa-moon" />
+					</span>
 					<input
 						type="checkbox"
 						id="themeSwitcher"
