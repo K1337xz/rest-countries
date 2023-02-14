0dee0ae65d0ce0bdcf009fa138cdbba46ea166cd
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-02-14 19:49:56 +0100

    Final mobile designe

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./assets/css/index.css";
 import Navbar from "./components/Navbar";
-
+import Footer from "./components/Footer";
 import Main from "./components/Main";
 
 export default function App() {
@@ -15,6 +15,7 @@ export default function App() {
 		<div className="app" data-theme={theme ? "dark" : "light"}>
 			<Navbar themeSwitcher={themeToggle} />
 			<Main />
+			<Footer />
 		</div>
 	);
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Footer() {
-	return <footer></footer>;
+	return (
+		<footer>
+			<div class="attribution">
+				Challenge by . Coded by
+				<a href="https://github.com/K1337xz"> Jakub B</a>.
+			</div>
+		</footer>
+	);
 }
